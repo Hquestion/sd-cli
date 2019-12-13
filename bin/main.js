ce96dba7225a28833f186d0de65c4bc1b7415d6d
hexiaolei <1035271660@qq.com>
2019-12-13 14:20:19 +0800

    add command register to sd-cli
    SDXUSER-280

--- bin/main.js
+++ bin/main.js
@@ -7,7 +7,7 @@ const ora = require('ora');
 const downloadGitRepo = require('download-git-repo');
 const { folderExist, getFullPath, rmPath } = require('../utils/fs');
 const { create } = require('../lib/project');
-const { register } = require('../lib/gateway');
+const { register, unregister } = require('../lib/gateway');
 
 const pack = require('../package');
 
@@ -33,6 +33,15 @@ program
     .option('-p --path <path>', '聚合接口脚本位置')
     .action(function(name, cmd) {
         register(name, cmd);
+    });
+
+program
+    .command('unregister')
+    .description('删除已注册的聚合接口')
+    .option('-n --name <name...>', '聚合接口函数名')
+    .option('-id --api-id <id...>', '聚合接口API_ID，如/fe-compose/api/v1/login或_fe-compose_api_v1_login')
+    .action((cmd) => {
+        unregister({name, apiId} = cmd);
     });
 
 program.parse(process.argv);
